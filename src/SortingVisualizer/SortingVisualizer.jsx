87a711ed8f4e09e58bf7e80950f00aff616bf60a
Mokarrom Rahman <mrahman0000@gmail.com>
2020-03-25 23:04:55 -0600

    Finished merge sort and selection sort. Wrote robust testing function and button. Tested merge sort and selection sort.

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -64,8 +64,73 @@ export default class SortingVisualizer extends React.Component
 
     selectionSort()
     {
+        //native js sorting algorithm for ordered nums
+        const jsSortedArray = this.state.array.slice().sort((a,b) => a - b);
+        const sortedArray = SortingAlgorithms.selectionSort(this.state.array);
+        //console.log('im working');
 
+        console.log(arraysAreEqual(jsSortedArray,sortedArray));
     }
+
+    superTest()
+    {
+        //use this for robust testing to make sure that the sorts are 
+        //perfect everytime
+        //<button onClick={() => this.superTest()}>Selection Sort</button>
+
+        let sortsWorking = "Algorithm is working";
+        //do 1000 tests
+        for(var i = 0; i < 1000; ++i)
+        {
+            let randomArraySize = Math.floor(Math.random()*1000 + 1);
+            let array = [];
+            //make a random array of the chosen size
+            //using values from 0 to 1000
+            for(let j = 0; j < randomArraySize; ++j)
+            {
+                array.push(randomIntFromInterval(-1000,1000));
+            }
+
+            //javacript's native sort used to test how this is working
+            let jsSortedArray = array.sort((a,b) => a - b);
+            //swap out this line
+            //let sortedArray = SortingAlgorithms.mergeSort(array);
+            let sortedArray = SortingAlgorithms.selectionSort(array);
+
+            //this was used to see what the individual value of each array was
+            // console.log("sortedArray length:" + sortedArray.length);
+            // console.log("jsSort length:" + jsSortedArray.length);
+            // console.log(array.length);
+             //for(let k = 0; k < jsSortedArray.length; ++k)
+             //{
+                 //console.log(jsSortedArray[k]);
+             //}
+            //const sortedArray = SortingAlgorithms.selectionSort(array);
+            // for(let k = 0; k < jsSortedArray.length; ++k)
+            // {
+            //     console.log(sortedArray[k]);
+            // }
+            console.log(randomArraySize + " sized array used");
+
+            //empty array
+            array = [];
+
+            //reassign and check it
+            if(!arraysAreEqual(jsSortedArray, sortedArray))
+            {
+                console.log(arraysAreEqual(jsSortedArray, sortedArray));
+                console.log("Algorithm broken!!!!!");
+                sortsWorking = "Algorithm is broken";
+                break;
+            }
+        }
+        
+        //print final result of the sort
+        console.log(i + "number of tests"); //print how many loops were run
+        console.log(sortsWorking);
+    }
+
+
     //each value of the array is mapped to
     render()
     {
@@ -91,6 +156,7 @@ export default class SortingVisualizer extends React.Component
             <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
             <button onClick={() => this.insertionSort()}>Insertion Sort</button>
             <button onClick={() => this.selectionSort()}>Selection Sort</button>
+            <button onClick={() => this.superTest()}>Super Test</button>
             </div>
             //the generate new button creates a new awway for us
         );
--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,6 +1,90 @@
-function mergeSort(array)
-{
+//export const getMergeSortANimations
+
+export const mergeSort = (array, animations = []) =>                         //O(nlogn) time complexity
+{                                                   //O(n) space complexity           
     //console.log('im working');
 
+    if(array.length === 1)
+    {
+        return array;
+    }
+
+    //find middle index
+    const middleIndex = Math.floor(array.length/2);
+    //take the first half and sort it
+    const firstHalf = mergeSort(array.slice(0, middleIndex));
+    //take the 2nd half and sort it
+    const secondHalf = mergeSort(array.slice(middleIndex));
+    //final sorted array
+    const sortedArray = [];
+    let i = 0;
+    let j = 0;
+
+    //look at both of the arrays and 
+    //compare and put the smaller number into 
+    //the sorted array
+    while(i < firstHalf.length && j < secondHalf.length)
+    {
+        //found the smaller in first half
+        //so add to the sorted array
+        if(firstHalf[i] < secondHalf[j])
+        {
+            sortedArray.push(firstHalf[i]);
+            i++;
+        }
+        //the smallers is in the 2nd half
+        //so add to the sorted array
+        else
+        {
+            sortedArray.push(secondHalf[j]);
+            j++;
+        }
+    }
+
+    //push remaining numbers from 
+    //first half into the sorted array
+    while(i < firstHalf.length)
+    {
+        sortedArray.push(firstHalf[i]);
+        i++;
+    }
+
+    //push remaining numbers from 
+    //first half into the sorted array
+    while(j < secondHalf.length)
+    {
+        sortedArray.push(secondHalf[j]);
+        j++;
+    }
+
+    //return the sorted array at each step
+    return sortedArray;
+}
+
+export const selectionSort = array =>
+{
+    for(let i = 0; i < array.length - 1; ++i)
+    {
+        //hold the index of the current min value
+        let  minIndex = i;
+        for(let j = i + 1; j < array.length; ++j)
+        {
+            //if the number at the current minIndex is 
+            //greater than j, then j is the current
+            //index of the smallest number
+            //there fore reassign minIndex
+            if(array[minIndex] > array[j])
+            {
+                minIndex = j;
+            }
+        }
+
+        //Swap the min value into the correct area
+        let temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+    }
+
+    //return the sorted array
     return array;
 }
